65516317a0a5d448087766aa0911f68491b34d12
Angel <zangelweb@gmail.com>
2021-03-18 18:24:03 -0600

    ag

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,50 @@
+import { 
+    Component, 
+    Input, 
+    Output, 
+    EventEmitter, 
+    OnChanges, 
+    SimpleChanges,
+    OnInit,
+    OnDestroy
+} from '@angular/core';
+
+import { Product } from '../product.model';
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html'
+})
+export class ProductComponent implements OnChanges, OnInit, OnDestroy{
+    @Input() product: Product;
+    @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+
+    constructor(){
+        console.log('1. constructor')
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('2. ngOnChanges')
+        console.log(changes)
+    }
+
+    ngOnInit() {
+        console.log('3. ngOnInit')
+    }
+
+    // ngDoCheck() {
+        
+    // }
+
+    ngOnDestroy() {
+        //Called once, before the instance is destroyed.
+        //Add 'implements OnDestroy' to the class.
+        console.log('4. ngOnDestroy')
+    }
+
+    addCart(){
+        console.log('Añadido');
+        this.productClicked.emit(this.product.id);
+    }
+}
